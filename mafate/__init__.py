99f89005f19ba05ae820d3f2037684a4c60b706f
David Saint-Martin <david.saint-martin@meteo.fr>
2018-11-07 19:31:14 +0100

    last finalization v0.1

--- mafate/__init__.py
+++ mafate/__init__.py
@@ -1,2 +1,6 @@
+version = '0.1'
+
+print('mafate version = '+version)
+
 from .utils import fld_year_avg, zon_year_avg, Id, define_CLIMAF_projects, add_expe_dict, dict_reanalyses, dict_expes_CMIP5_piControl, dict_expes_CMIP5_historical, dict_expes_CMIP5_abrupt4xCO2, dict_expes_stab_article, dict_expes_historical_CNRMCM, dict_var, dict_vars_T, dict_vars_NT, dict_vars_heatc, load_datas, load_HadCRU_data
 from .varexpe import Expe, Variable
